3532d50e9a57d03dcde59078a1f586f4ce704a5d
Henri Susanto <henry.dinus@gmail.com>
2020-08-14 04:03:03 +0700

    login page title

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PrototypeApp | Log in</title>
+  <title>e-CSMS</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="<?= base_url('manifest.json') ?>" rel="manifest">
